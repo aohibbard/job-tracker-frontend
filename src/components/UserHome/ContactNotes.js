59b44fb28f113f286de758c4b796fe4148782998
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-04-16 21:06:10 -0400

    upcoming dealines almost works

--- src/components/UserHome/ContactNotes.js
+++ src/components/UserHome/ContactNotes.js
@@ -4,6 +4,21 @@ import Moment from 'react-moment';
 
 
 class ContactNotes extends React.Component{
+
+    //First Set up params for current date
+    //Array needs to function with date.
+    //Date needs to function with week
+    //Week needs to begin Monday, end Sunday. Current date needs to process its relation to prev. Mon & upcoming Sun
+
+    //Second iterate over most recent updates
+    //this.props.sort by last updated, desc
+    //commit these to an array
+    //if array.length <= 8
+    //you still need to talk to 8 - array.length people
+    //else you have spoken to everyone you need to
+
+    //evaluate array against set up params
+
     render(){
         console.log(this.props)
         // debugger
--- src/components/UserHome/JobNotes.js
+++ src/components/UserHome/JobNotes.js
@@ -1,17 +1,43 @@
 import React from 'react'
-import Moment from 'react-moment';
+import moment from 'moment';
 // <Moment format="dddd, MMMM Do YYYY">{this.props.job.due}</Moment></p>
 
 
+// this.props.jobs.jobs.forEach(job => job.due =>
+//     if (moment(job.due).isBetween(today, date){
+//         upcomingDeadlines.push(job)
+    
+// })
+
 class JobNotes extends React.Component{
+
+    //use moment to check if any deadlines are coming up in the next 7 days
+    //sort by jobs that have not been applied to
+
+    upcomingDates(){
+        let upcomingDeadlines = []
+        //should declare a date 7 days from moment.now
+        const today = moment(new Date());
+        const date = moment().day(7);
+        debugger
+        for (const job of this.props.jobs.jobs){
+            let dueDate = moment(job.due);
+            debugger
+            if(dueDate.isBeteween(today, date)){
+                upcomingDeadlines.push(job)
+            }
+        }   
+    }
+    
+
     render(){
         console.log(this.props)
-        debugger
         // debugger to play with contacts
         //decide what the logic is
         return(
             <div>
-                Job Notes 
+                Job Notes
+                <button onClick={this.upcomingDates()}>See Deadlines</button>
             </div>
         )
     }
